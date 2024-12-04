d1fc839058905384b9fe3640f244bf5bc4411169
emperorofmars <mars@squirrel.pub>
2024-12-04 03:58:45 +0100

    mpl fully implemented

--- nna/components/vrc/vrc_controller_mapping.py
+++ nna/components/vrc/vrc_controller_mapping.py
@@ -1,10 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import bpy
 
 from ...nna_registry import NNAFunctionType
 
-from ... import nna_utils_name
 from ... import nna_utils_json
-from ... import nna_utils_tree
 
 _nna_name = "vrc.controller_mapping"
 
