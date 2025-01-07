e1a9568c678dbd39bf8c9a68af82e6e806f57f3d
emperorofmars <mars@squirrel.pub>
2025-01-07 01:37:22 +0100

    draw vrm spring bones

--- nna/components/vrm/vrm_springbone.py
+++ nna/components/vrm/vrm_springbone.py
@@ -5,3 +5,16 @@ from ...ops import nna_operators_selector
 
 
 _nna_name = "vrm.springbone"
+
+
+def display_vrm_spring_component(target_id: str, layout: bpy.types.UILayout, json_component: dict, component_index: int):
+	nna_id_list.draw_id_list(target_id, layout, json_component, component_index, "colliders")
+
+	row = layout.split(factor=0.4); row.label(text="Raw Springbone Values"); row.label(text=str(json_component.get("parsed")))
+
+
+nna_types = {
+	_nna_name: {
+		NNAFunctionType.JsonDisplay: display_vrm_spring_component,
+	},
+}
